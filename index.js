23e5647f4e9f539fe6be6acb3c07c4f21f802c09
Joshua Norman <joshuadnorman@gmail.com>
2022-06-19 19:16:52 +0200

    Make Node a class , make a tree class with a root value which is a node. Node consists of left and a right node. Added toString to Tree class which begins at the root and builds the arithmetic expression recursively

--- index.js
+++ index.js
@@ -1,62 +1,98 @@
 const assert = require("assert");
 
-const Node = (operator, value, left, right) => {
-  const result = function () {
-    switch (this.operator) {
-      case "+":
-        return left.result() + right.result();
-      case "-":
-        return left.result() - right.result();
-      case "x":
-        return left.result() * right.result();
-      case "÷":
-        return left.result() / right.result();
-      default:
-        return value;
-    }
-  };
+// const Node = (operator, value, left, right) => {
+//   const result = function () {
+//     switch (this.operator) {
+//       case "+":
+//         return left.result() + right.result();
+//       case "-":
+//         return left.result() - right.result();
+//       case "x":
+//         return left.result() * right.result();
+//       case "÷":
+//         return left.result() / right.result();
+//       default:
+//         return value;
+//     }
+//  };
 
-  const toString = function () {
-    switch (this.operator) {
-      case "+":
-        return `(${left.toString()} + ${right.toString()})`;
-      case "-":
-        return `(${left.toString()} - ${right.toString()})`;
-      case "x":
-        return `(${left.toString()} x ${right.toString()})`;
-      case "÷":
-        return `(${left.toString()} ÷ ${right.toString()})`;
-      default:
-        return value.toString();
-    }
-  };
+//   const toString = function () {
+//     switch (this.operator) {
+//       case "+":
+//         return `(${left.toString()} + ${right.toString()})`;
+//       case "-":
+//         return `(${left.toString()} - ${right.toString()})`;
+//       case "x":
+//         return `(${left.toString()} x ${right.toString()})`;
+//       case "÷":
+//         return `(${left.toString()} ÷ ${right.toString()})`;
+//       default:
+//         return value.toString();
+//     }
+//   };
 
-  return {
-    operator,
-    value,
-    left,
-    right,
-    result,
-    toString
-  };
+//   return {
+//     operator,
+//     value,
+//     left,
+//     right,
+//     result,
+//     toString
+//   };
+// };
+
+
+class Node{
+    constructor(operator,value,left,right){
+        this.operator = operator;
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Tree{
+    constructor(data){
+        this.root = data;
+    }
+
+    toString = function(node=this.root){
+        if (node==null) return
+            switch (node.operator) {
+                case "+":
+                    return `(${this.toString(node.left)} + ${this.toString(node.right)})`;
+                case "-":
+                    return `(${this.toString(node.left)} - ${this.toString(node.right)})`;
+                case "x":
+                    return `(${this.toString(node.left)} x ${this.toString(node.right)})`;
+                case "÷":
+                    return `(${this.toString(node.left)} ÷ ${this.toString(node.right)})`;
+                default:
+                    return node.value.toString();
+    }
+}
 };
 
-const tree = Node(
+const tree = new Tree(
+new Node(
   "÷",
   null,
-  Node(
+  new Node(
     "+",
     null,
-    Node("", 7, null, null),
-    Node(
+    new Node("", 7, null, null),
+    new Node(
       "x",
       null,
-      Node("-", null, Node("", 3, null, null), Node("", 2, null, null)),
-      Node("", 5, null, null)
+      new Node("-", null, new Node("", 3, null, null), new Node("", 2, null, null)),
+      new Node("", 5, null, null)
     )
   ),
-  Node("", 6, null, null)
+  new Node("", 6, null, null)
+)
 );
 
-assert.strictEqual("((7 + ((3 - 2) x 5)) ÷ 6)", tree.toString());
-assert.strictEqual(2, tree.result());
+
+console.log(tree.toString())
+//assert.strictEqual("((7 + ((3 - 2) x 5)) ÷ 6)", tree.toString());
+//assert.strictEqual(2, tree.result());
